fcf44d8fb2e1bef839d62c4559d7d7f5c823079e
themnchstr <themnchstr@gmail.com>
2022-12-02 13:59:27 +0500

    Add Lesson_6 Hello name

---
+++ Lesson_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "anton")
+{
+    Console.WriteLine("URAAA");
+}
+else
+{
+    Console.WriteLine("Hi, ");
+    Console.WriteLine(username);
+}
